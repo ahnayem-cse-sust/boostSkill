831f7e327662220c277c7e103b2ad0c025f89c56
Abu hanife nayem <ahnayem.sust@gmail.com>
2024-09-07 22:33:01 +0600

    log in page

---
+++ resources/js/Components/User/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">BoostSkill</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                
+            </ul>
+            <ul class="navbar-nav mb-2 mb-lg-0 ">
+                <li class="nav-item">
+                    <Link :href="route('profile.edit')"> Profile </Link>
+                    <Link :href="route('logout')" method="post" as="button">
+                        Log Out
+                    </Link>
+                </li>
+            </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script setup>
+import {Link} from '@inertiajs/vue3';
+
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,10 +1,4 @@
 <script setup>
-import Checkbox from '@/Components/Checkbox.vue';
-import GuestLayout from '@/Layouts/GuestLayout.vue';
-import InputError from '@/Components/InputError.vue';
-import InputLabel from '@/Components/InputLabel.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
-import TextInput from '@/Components/TextInput.vue';
 import { Head, Link, useForm } from '@inertiajs/vue3';
 
 defineProps({
@@ -23,6 +17,7 @@ const form = useForm({
 });
 
 const submit = () => {
+    console.log('FFFF');
     form.post(route('login'), {
         onFinish: () => form.reset('password'),
     });
@@ -30,7 +25,65 @@ const submit = () => {
 </script>
 
 <template>
-    <GuestLayout>
+    <Head title="Log in" />
+
+    <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
+        {{ status }}
+    </div>
+    <section class="vh-100">
+        <div class="container-fluid">
+            <div class="row">
+            <div class="col-sm-6 text-black">
+
+                <div class="px-5 ms-xl-4">
+                <i class="fas fa-crow fa-2x me-3 pt-5 mt-xl-4" style="color: #709085;"></i>
+                <span class="h1 fw-bold mb-0">Logo</span>
+                </div>
+
+                <div class="d-flex align-items-center h-custom-2 px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5">
+
+                <form style="width: 23rem;" @submit.prevent="submit">
+
+                    <h3 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Log in</h3>
+
+                    <div data-mdb-input-init class="form-outline mb-4">
+                    <input v-model="form.email" type="email" id="email" class="form-control form-control-lg" />
+                    <label class="form-label" for="form2Example18">Email address</label>
+                    </div>
+
+                    <div data-mdb-input-init class="form-outline mb-4">
+                    <input v-model="form.password" type="password" id="password" class="form-control form-control-lg" />
+                    <label class="form-label" for="form2Example28">Password</label>
+                    </div>
+
+                    <div class="pt-1 mb-4">
+                    <button data-mdb-button-init data-mdb-ripple-init class="btn btn-info btn-lg btn-block" type="submit">Login</button>
+                    </div>
+
+                    <p class="small mb-5 pb-lg-2">
+                        <Link
+                            v-if="canResetPassword"
+                            :href="route('password.request')"
+                            class=""
+                        >
+                            Forgot your password?
+                        </Link>
+                    </p>
+                    <p>Don't have an account? <a href="#!" class="link-info">Register here</a></p>
+
+                </form>
+
+                </div>
+
+            </div>
+            <div class="col-sm-6 px-0 d-none d-sm-block">
+                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img3.webp"
+                alt="Login image" class="w-100 vh-100" style="object-fit: cover; object-position: left;">
+            </div>
+            </div>
+        </div>
+    </section>
+    <!-- <GuestLayout>
         <Head title="Log in" />
 
         <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
@@ -90,5 +143,21 @@ const submit = () => {
                 </PrimaryButton>
             </div>
         </form>
-    </GuestLayout>
+    </GuestLayout> -->
 </template>
+
+<style scoped>
+.bg-image-vertical {
+position: relative;
+overflow: hidden;
+background-repeat: no-repeat;
+background-position: right center;
+background-size: auto 100%;
+}
+
+@media (min-width: 1025px) {
+.h-custom-2 {
+height: 100%;
+}
+}
+</style>
