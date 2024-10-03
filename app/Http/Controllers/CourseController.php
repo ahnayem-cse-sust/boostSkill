4e9162195fab75fb43c425f01a2cf52b0207a58d
Abu hanife nayem <ahnayem.sust@gmail.com>
2024-10-04 00:11:48 +0600

    course

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -2,9 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\CourseService;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class CourseController extends Controller
 {
-    //
+    private $service;
+    public function __construct(CourseService $service)
+    {
+        parent::__construct();
+        $this->service = $service;
+    }
+    
+    public function setModuleName()
+    {
+        $this->moduleName = MODULE_NAME_COURSE;
+    }
+
+    public function create(){
+        if($this->hasPermission(PERMISSION_TYPE_CREATE)){
+            return Inertia::render('Admin/Course/Create', [
+                'status' => session('status'),
+            ]);
+        } else{
+            return $this->unauthorizedPageResponse();
+        }
+    }
+
+    public function store(Request $request){
+        dd('ffff');
+        if($this->hasPermission(PERMISSION_TYPE_CREATE)){
+            $this->service->store($request);
+        } else{
+            return $this->unauthorizedPageResponse();
+        }
+    }
 }
---
+++ app/Services/CourseService.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace App\Services;
+
+use App\Models\Course;
+use App\Models\CourseCategory;
+use App\Models\EnrolledCourse;
+
+
+class CourseService extends BaseService
+{
+
+    public function __construct()
+    {
+        
+    }
+
+    public function store(){
+        dd("NNNN");
+    }
+
+
+}
