4fb30a1fe3c090bd165f2f1c78a836492dfbb665
Abu hanife nayem <ahnayem.sust@gmail.com>
2024-11-04 06:43:16 +0600

    payment & enrollment migration

---
+++ database/migrations/2024_11_03_140542_create_enrollments_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('enrollments', function (Blueprint $table) {
+            $table->id();
+            $table->integer('course_id')->nullable();
+            $table->integer('student_id')->nullable();
+            $table->string('status')->nullable();
+            $table->integer('total_paid')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('enrollments');
+    }
+};
---
+++ database/migrations/2024_11_04_002831_create_payments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->integer('customer_id')->nullable();
+            $table->string('payment_type')->nullable();
+            $table->string('status')->nullable();
+            $table->string('payment_objective')->nullable();
+            $table->integer('payment_objective_id')->nullable();
+            $table->integer('amount')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
